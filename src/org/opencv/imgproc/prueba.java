a8df8f94e2ac4acf2988fe3b0693944d68a606c7
Sergio <Sergio@DESKTOP-HQLEEF6>
2016-06-08 17:58:32 -0500

    Dame la dona

--- src/org/opencv/imgproc/prueba.java
+++ src/org/opencv/imgproc/prueba.java
@@ -32,51 +32,104 @@ public class prueba {
 }
 class Procesar {
     
-    Mat imagen,imagenCopia;
-    int width,height;
-     Mat matriz ;
+    private Mat imagen,imagenCopia;
+    private int width,height;
 
     public Procesar(){
 
         imagen = Highgui.imread("C:\\Users\\Sergio\\Documents\\TT\\imagenes\\mio.jpg",Highgui.CV_LOAD_IMAGE_COLOR);
          imagenCopia = Highgui.imread("C:\\Users\\Sergio\\Documents\\TT\\imagenes\\mio.jpg",Highgui.CV_LOAD_IMAGE_COLOR);
         if(!imagen.empty()){
-            
-            /*Imagen original*/
-            Image imagenMostrar = convertir(imagen);
-            Ventana ventana = new Ventana(imagenMostrar,0,0);
-
-            imagen=descompCanImg(imagen,2) ;
-            Ventana ventana3 = new Ventana(convertir(imagen),1,0);
-            
-            /*Umbralizado*/
-            imagen=umbralizarImg(imagen) ;
-            Ventana ventana4 = new Ventana(convertir(imagen),2,0);
              
-            /* Erosionar*/
-            imagen=eriosionarImg(imagen) ;
-            Ventana ventana5 = new Ventana(convertir(imagen),0,1);
+             
+            Ventana v= new Ventana (convertir(dameLaDona(imagen)),0,0) ; 
             
-            /* Erosionar*/
-            imagen=dilatarImg(imagen) ;
-            Ventana ventana6 = new Ventana(convertir(imagen),1,1);
-            
-            /*Canny*/
-            imagen=filtroCanny(imagen) ;
-            Ventana ventana7 = new Ventana(convertir(imagen),2,1);
-            
-             /*Transformada Hough*/
-            Vector<Mat> circlesList=TransfHough(imagen) ;
-            System.out.println(circlesList);
-            
-    
             
         }else{
             System.out.println("\tImagen NO encontrada");
         }
     }
- 
-    private Image convertir(Mat imagen) {
+     
+    private Mat dameLaDona(Mat img1) {
+        double [] iris= segmentarIris(img1) ;
+        double [] pupila=segmentarPupila(img1) ;
+        
+        Size sizeA = img1.size();
+        Mat  max=img1.clone() ;
+        
+        double[] data ;
+        for (int i = 0; i < sizeA.height; i++)
+            for (int j = 0; j < sizeA.width; j++) {
+                 if ((Math.pow(iris[0]-j,2)+Math.pow(iris[1]-i,2)>= Math.pow(iris[2],2)) || (Math.pow(pupila[0]-j,2)+Math.pow(pupila[1]-i,2)<= Math.pow(pupila[2],2)) )
+                    {   data = img1.get(i, j);
+                        data[0]=255 ;
+                        data[1]=255 ;
+                        data[2]=255 ;
+                        max.put(i, j, data);
+                    }
+       
+    }
+        return max;
+    }
+    
+     public double[] segmentarIris(Mat img){
+            /*Imagen original*/
+            Image imagenMostrar = convertir(img);
+            Ventana ventana = new Ventana(imagenMostrar,0,0);
+            /*Descomponer canales*/
+            img=descompCanImg(img,1) ;
+            Ventana ventana3 = new Ventana(convertir(img),1,0);
+            /*Filtro Gauss*/
+            img=blurearImg(img,25) ;
+            Ventana ventana31 = new Ventana(convertir(img),2,0);
+            /*Umbralizado*/
+            img=umbralizarImg(img,50,100) ;
+            Ventana ventana4 = new Ventana(convertir(img),0,1);
+            /* Erosionar*/
+            img=eriosionarImg(img) ;
+            Ventana ventana5 = new Ventana(convertir(img),1,1);
+            /* Erosionar*/
+            img=dilatarImg(img) ;
+            Ventana ventana6 = new Ventana(convertir(img),2,1);
+            /*Canny*/
+            //imagen=filtroCanny(imagen) ;
+            img=umbralizarImg(img,0,255) ;
+            Ventana ventana7 = new Ventana(convertir(img),0,2);
+             /*Transformada Hough*/
+            return TransfHough(img,1, 200, 1, 10, 100, 150) ;
+     }
+     public double[] segmentarPupila(Mat img){
+         /*Imagen original*/
+            Image imagenMostrar = convertir(img);
+            Ventana ventana = new Ventana(imagenMostrar,0,0);
+
+            img=descompCanImg(img,2) ;
+            Ventana ventana3 = new Ventana(convertir(img),1,0);
+            
+            /*Umbralizado*/
+            img=umbralizarImg(img,20,100) ;
+            Ventana ventana4 = new Ventana(convertir(img),2,0);
+             
+            /* Erosionar*/
+            img=eriosionarImg(img) ;
+            Ventana ventana5 = new Ventana(convertir(img),0,1);
+            
+            /* Erosionar*/
+            img=dilatarImg(img) ;
+            Ventana ventana6 = new Ventana(convertir(img),1,1);
+            
+            /*Canny*/
+            //imagen=filtroCanny(imagen) ;
+            img=umbralizarImg(img,0,255) ;
+            Ventana ventana7 = new Ventana(convertir(img),2,1);
+            
+            
+             /*Transformada Hough*/
+             
+            //System.out.println(circlesList);
+            return TransfHough(img,1, 200, 1, 10, 20, 50)  ;
+     }
+     private Image convertir(Mat imagen) {
         MatOfByte matOfByte = new MatOfByte();
         Highgui.imencode(".jpg", imagen, matOfByte);
  
@@ -92,23 +145,23 @@ class Procesar {
         }
         return (Image)bufImage;
     }
-    
-     private Mat umbralizarImg(Mat canalRojo) {
-        Imgproc.threshold(canalRojo, canalRojo, 20, 60, Imgproc.THRESH_BINARY);
+     private Mat umbralizarImg(Mat canalRojo,int min, int max) {
+        Imgproc.threshold(canalRojo, canalRojo, min, max, Imgproc.THRESH_BINARY);
         return canalRojo;
     }
-     
+     private Mat blurearImg(Mat canalAzul,int val) {
+        Imgproc.medianBlur(canalAzul, canalAzul, val);
+        return canalAzul;
+    }
      private Mat escalaGrayImg(Mat img) {
         Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2GRAY);
         return img;
     }
-     
      private Mat descompCanImg(Mat grayscale,int canal) {
          List<Mat> rgb = new ArrayList<Mat>(3);
          Core.split(grayscale,rgb);
         return rgb.get(canal);
     }
-    
      private Mat eriosionarImg(Mat umbralizada) {
             Mat dste = umbralizada.clone();
             int erosion_size = 5;
@@ -127,85 +180,54 @@ class Procesar {
             Imgproc.dilate(umbralizada, dstd, elementd);
         return dstd;
     }
-     
-      private Mat filtroCanny(Mat segmentada) {
+     private Mat filtroCanny(Mat segmentada) {
           int umbralmin=32;
           int umbralmax=100;            
             Imgproc.Canny(segmentada, segmentada, umbralmin,umbralmax);
             
         return segmentada;
-    }
-     /* private Mat TransfHough(Mat segmentada) {       
-            Mat circles = new Mat();
-           Imgproc.HoughCircles(segmentada, circles, Imgproc.CV_HOUGH_GRADIENT,1,60, 200, 20, 30, 0);
-           //Imgproc.minEnclosingCircle(null, null, radius);
-            System.out.println("#rows " + circles.rows() + " #cols " + circles.cols());
-    double x = 0.0;
-    double y = 0.0;
-    int r = 0;
-
-    for( int i = 0; i < circles.rows(); i++ )
-    {
-      double[] data = circles.get(i, 0);
-      for(int j = 0 ; j < data.length ; j++){
-           x = data[0];
-           y = data[1];
-           r = (int) data[2];
-      }
-      Point center = new Point(x,y);
-      // circle center
-      Core.circle( segmentada, center, 3, new Scalar(0,255,0), -1);
-      // circle outline
-      Core.circle( segmentada, center, r, new Scalar(0,0,255), 1);
-
-        
-       }       
-            return segmentada;
-      }*/
-      
-       public Vector<Mat> TransfHough(Mat edges){
-     
-           Ventana ventana9 = new Ventana(convertir(imagenCopia),0,2);
+    } 
+     public double[] TransfHough(Mat edges,int inv, int distMinCir,int umbMin, int umbMax, int radMin, int radMax){
+ 
            Mat color = imagenCopia;
-          edges= descompCanImg(imagenCopia,2) ;
-        
-    Mat circles = new Mat();
-    Vector<Mat> circlesList = new Vector<Mat>();
+           Mat circles = new Mat();
+           double[] data=null ;
+           //Vector<Mat> circlesList = new Vector<Mat>();
+           Imgproc.HoughCircles(edges, circles, Imgproc.CV_HOUGH_GRADIENT,inv,distMinCir,umbMin,umbMax,radMin,radMax);
+      //Imgproc.HoughCircles(edges, circles, Imgproc.CV_HOUGH_GRADIENT, 1, 200, 1, 10, 20, 40  );
+    //Imgproc.HoughCircles(edges, circles, Imgproc.CV_HOUGH_GRADIENT, 1, 50) ;
+    
+           System.out.println("#rows " + circles.rows() + " #cols " + circles.cols());   
+           double x = 0.0;
+           double y = 0.0;
+           int r = 0;
 
-
-      Imgproc.HoughCircles(edges, circles, Imgproc.CV_HOUGH_GRADIENT, 1, 200, 1, 30, 20, 40  );
-    //Imgproc.HoughCircles(edges, circles, Imgproc.CV_HOUGH_GRADIENT, 1, 1) ;
-      System.out.println("#rows " + circles.rows() + " #cols " + circles.cols());   
-      
-    double x = 0.0;
-    double y = 0.0;
-    int r = 0;
-
-    for( int i = 0; i < circles.rows(); i++ )
-    {
-      double[] data = circles.get(i, 0);
-      for(int j = 0 ; j < data.length ; j++){
-           x = data[0];
-           y = data[1];
-           r = (int) data[2];
-      }
-      Point center = new Point(x,y);
+           for( int i = 0; i < circles.rows(); i++ )
+            {
+                data = circles.get(i, 0);
+                for(int j = 0 ; j < data.length ; j++)
+                {
+                    x = data[0];
+                    y = data[1];
+                    r = (int) data[2];
+                }
+                Point center = new Point(x,y);
+                
       // circle center
-      Core.circle( color, center, 2, new Scalar(255,0,0), -1);
+                Core.circle( color, center, 1, new Scalar(0,0,0), -1);
       // circle outline
-      Core.circle( color, center, r, new Scalar(0,255,0), 1);
-
-     Ventana ventana8 = new Ventana(convertir(color),1,2);
+                Core.circle( color, center, r, new Scalar(0,255,0), 1);
+                Ventana ventana8 = new Ventana(convertir((color)),0,2);
 
      /*
-     Rect bbox = new Rect((int)Math.abs(x-r), (int)Math.abs(y-r), (int)2*r, (int)2*r);
-     Mat croped_image = new Mat(edges, bbox);
-     Imgproc.resize(croped_image, croped_image, new Size(160,160));
-     circlesList.add(croped_image);
-     //Ventana ventana10 = new Ventana(convertir(edges),2,2);
-             */
-    }
+            Rect bbox = new Rect((int)Math.abs(x-r), (int)Math.abs(y-r), (int)2*r, (int)2*r);
+            Mat croped_image = new Mat(color, bbox);
+            Imgproc.resize(croped_image, croped_image, new Size(200,200));
+            circlesList.add(croped_image);
+            Ventana ventana9 = new Ventana(convertir(croped_image),1,2);
+       */      
+            }
 
-    return circlesList;
-}
+    return data;
+    }
 }
